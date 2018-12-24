4435fccbc89016f4ef25215d7ce1c022cee72c6c
Punit Kunjam <punit17100@iiitnr.edu.in>
2018-12-25 00:28:34 +0530

    ex3

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -37,12 +37,18 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X * theta);
 
+J = 1/m * sum(-y .* log(h) - (1-y) .* log(1-h));
 
+J = J + (lambda/(2*m)) * sum(theta(2:end).^2);
 
+grad = 1/m * ( X' * (h-y)) ;
 
+temp = theta;
+temp(1) = 0;
 
-
+grad = grad + (lambda/m)*temp;
 
 
 % =============================================================
